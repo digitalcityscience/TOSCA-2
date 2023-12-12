d9ab716d62fdb04cf28a309542355f1313046501
Gokturk Kose <gokturkbrk@gmail.com>
2023-12-12 11:39:06 +0100

    feat: add primevue

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
 import './style.css'
 import App from './App.vue'
 
 const pinia = createPinia()
 
-createApp(App).use(pinia).mount('#app')
+createApp(App).use(pinia).use(PrimeVue).mount('#app')
