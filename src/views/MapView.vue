2ab1f66c4c20c47647c665dae504f6dc88b18f9c
Gokturk Kose <gokturkbrk@gmail.com>
2023-12-18 17:06:52 +0100

    feat: create mapview component
    New MapView component created for seperate layout and logic from App.vue

---
+++ src/views/MapView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="mapview">
+        <MapContainer></MapContainer>
+    </div>
+</template>
+
+<script setup lang="ts">
+import MapContainer from '../components/MapContainer.vue'
+</script>
+
+<style scoped>
+.mapview{
+    width:100%;
+    height:100%;
+}
+</style>
