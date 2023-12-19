3721f7d14acf5067207dc9f740e8c7f9f3d5e66d
Gokturk Kose <gokturkbrk@gmail.com>
2023-12-19 11:39:00 +0100

    add geoserver store
    Currently we are using basic authentication to create connection with geoserver. It can be changed later.

---
+++ src/store/geoserver.ts
@@ -0,0 +1,178 @@
+import { defineStore, acceptHMRUpdate } from 'pinia'
+import { ref } from 'vue'
+export interface GeoServerFeatureType {
+    featureType: {
+      name: string;
+      nativeName: string;
+      namespace: {
+        name: string;
+        href: string;
+      };
+      title: string;
+      abstract: string;
+      keywords: {
+        string: string[];
+      };
+      nativeCRS: string;
+      srs: string;
+      nativeBoundingBox: {
+        minx: number;
+        maxx: number;
+        miny: number;
+        maxy: number;
+        crs: string;
+      };
+      latLonBoundingBox: {
+        minx: number;
+        maxx: number;
+        miny: number;
+        maxy: number;
+        crs: string;
+      };
+      projectionPolicy: string;
+      enabled: boolean;
+      store: {
+        "@class": string;
+        name: string;
+        href: string;
+      };
+      serviceConfiguration: boolean;
+      simpleConversionEnabled: boolean;
+      internationalTitle: string;
+      internationalAbstract: string;
+      maxFeatures: number;
+      numDecimals: number;
+      padWithZeros: boolean;
+      forcedDecimal: boolean;
+      overridingServiceSRS: boolean;
+      skipNumberMatched: boolean;
+      circularArcPresent: boolean;
+      attributes: {
+        attribute: Array<{
+          name: string;
+          minOccurs: number;
+          maxOccurs: number;
+          nillable: boolean;
+          binding: string;
+        }>;
+      };
+    };
+  }
+export interface GeoserverLayerInfo{
+        name: string;
+        type: string;
+        defaultStyle: {
+          name: string;
+          href: string;
+        };
+        resource: {
+          "@class": string;
+          name: string;
+          href: string;
+        };
+        attribution: {
+          logoWidth: number;
+          logoHeight: number;
+        };
+        dateCreated: string;
+        dateModified: string;
+}
+export interface GeoserverLayerListItem {
+    name:string
+    href:string
+}
+export interface WorkspaceListItem{
+    name:string
+    href:string
+}
+export const useGeoserverStore = defineStore('geoserver', () => {
+
+ const pointData = ref()
+ const auth = btoa(`${import.meta.env.VITE_GEOSERVER_USERNAME+':'+import.meta.env.VITE_GEOSERVER_PASSWORD}`)
+ const layerList = ref<Array<GeoserverLayerListItem>>()
+ const workspaceList = ref<Array<WorkspaceListItem>>()
+ /**
+  * Gets layer list from geoserver. If optional workspace argument used it returns only layer list under this workspace.
+  * @returns
+  */
+ async function getLayerList(workspaceName?:string):Promise<JSON>{
+    let url = new URL(`${import.meta.env.VITE_GEOSERVER_REST_URL}/layers`)
+    if(workspaceName){
+        url = new URL(`${import.meta.env.VITE_GEOSERVER_REST_URL}/workspaces/${workspaceName}/layers`)
+    }
+    const response = await fetch(url,{ 
+        method:'GET',
+        redirect:'follow',
+        headers: new Headers({
+            'Content-Type':'application/json',
+            'Authorization': `Basic ${auth}`
+        }) 
+    })
+    return response.json()
+ }
+ /**
+  * Lists all workspaces user has access in geoserver.
+  * @returns
+  */
+ async function getWorkspaceList():Promise<JSON>{
+    const url = new URL(`${import.meta.env.VITE_GEOSERVER_REST_URL}/workspaces`)
+    const response = await fetch(url,{ 
+        method:'GET',
+        redirect:'follow',
+        headers: new Headers({
+            'Content-Type':'application/json',
+            'Authorization': `Basic ${auth}`
+        }) 
+    })
+    return response.json()
+ }
+/**
+ * Retrieves layer information based on GeoserverLayerInfo.
+ *
+ * @param layer - The layer for which to retrieve information.
+ * @param workspace - The workspace in which the layer is located.
+ * @returns - A Promise resolving to the JSON representation of the layer information.
+ */
+ async function getLayerInformation(layer:GeoserverLayerListItem,workspace:string):Promise<JSON> {
+    const url = new URL(`${import.meta.env.VITE_GEOSERVER_REST_URL}/workspaces/${workspace}/layers/${layer.name}`)
+    const response = await fetch(url,{ 
+        method:'GET',
+        redirect:'follow',
+        headers: new Headers({
+            'Content-Type':'application/json',
+            'Authorization': `Basic ${auth}`
+        }) 
+    })
+    return response.json()
+ }
+ /**
+ * Retrieves detailed layer information.
+ *
+ * @param url - Resource.href from GeoserverLayerInfo
+ * @returns - A Promise resolving to the JSON representation of the layer detailed information.
+ */
+ async function getLayerDetail(url:string):Promise<JSON> {
+    const response = await fetch(url,{
+        method:'GET',
+        redirect:'follow',
+        headers: new Headers({
+            'Content-Type':'application/json',
+            'Authorization': `Basic ${auth}`
+        }) 
+    })
+    return response.json()
+ }
+ return {
+    pointData,
+    layerList,
+    workspaceList,
+    getLayerList,
+    getWorkspaceList,
+    getLayerInformation,
+    getLayerDetail
+ }
+})
+
+if (import.meta.hot) {
+ import.meta.hot.accept(acceptHMRUpdate(useGeoserverStore, import.meta.hot))
+}
