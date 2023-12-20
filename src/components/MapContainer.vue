24712bbd8b3dc1e84d7789936b33aedaf5fc0ba6
Gokturk Kose <gokturkbrk@gmail.com>
2023-12-20 13:07:23 +0100

    feat: move map to the mapstore

--- src/components/MapContainer.vue
+++ src/components/MapContainer.vue
@@ -6,8 +6,11 @@
 <script setup lang="ts">
 import maplibre from 'maplibre-gl'
 import { onMounted } from 'vue';
+import { useMapStore } from '../store/map';
+
+const mapStore = useMapStore()
 onMounted(()=>{
-  new maplibre.Map({
+  mapStore.map = new maplibre.Map({
     container: 'map',
     style: `https://api.maptiler.com/maps/a2eb63ba-7d0e-4b25-9cfc-9ef74d786ec4/style.json?key=${import.meta.env.VITE_MAPTILER_API_KEY}`, // stylesheet location
     center: [9.993163, 53.552123], // starting position [lng, lat]
