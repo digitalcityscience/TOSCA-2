91bc7a45fcbfbdf70df3141d1bbc61cb30dfbf2d
Gokturk Kose <gokturkbrk@gmail.com>
2024-03-05 17:21:51 +0100

    add unstyled presets for theming
    from now on, we are removing premade styling for primevue components. to begin with we are going to implement presets from primevue for basic styling with tailwindcss.
    for more information about styling with tailwindcss and presets please visit:
     https://tailwind.primevue.org/overview/

---
+++ src/presets/tosca/index.d.ts
@@ -0,0 +1,18 @@
+export interface Preset {
+    global: any;
+    dropdown: any;
+    button: any;
+    selectbutton: any;
+    card: any;
+    radiobutton: any;
+    overlaypanel: any;
+    inputtext: any;
+    panel: any;
+    colorpicker: any;
+    slider: any;
+    checkbox: any;
+    accordion: any;
+    inlinemessage: any;
+}
+declare const toscaPresets: Preset;
+export default toscaPresets;
---
+++ src/presets/tosca/index.js
@@ -0,0 +1,32 @@
+import global from "./global.js";
+import dropdown from "./dropdown/index.js";
+import button from "./button/index.js";
+import selectbutton from "./selectbutton/index.js";
+import card from "./card/index.js";
+import radiobutton from "./radiobutton/index.js";
+import overlaypanel from "./overlaypanel/index.js";
+import inputtext from "./inputtext/index.js";
+import panel from "./panel/index.js";
+import colorpicker from "./colorpicker/index.js";
+import slider from "./slider/index.js";
+import checkbox from "./checkbox/index.js";
+import accordion from "./accordion/index.js";
+import inlinemessage from "./inlinemessage/index.js";
+
+
+export default {
+    global,
+    dropdown,
+    button,
+    selectbutton,
+    card,
+    radiobutton,
+    overlaypanel,
+    inputtext,
+    panel,
+    colorpicker,
+    slider,
+    checkbox,
+    accordion,
+    inlinemessage
+}
