81726fa9b38764979329960583a65e81b99610ce
Gokturk Kose <gokturkbrk@gmail.com>
2024-06-03 11:13:02 +0200

    change feature's attribute listing ui in attribute dialog

--- src/components/MapAttributeDialog.vue
+++ src/components/MapAttributeDialog.vue
@@ -1,25 +1,28 @@
 <template>
-	<div class="w-full text-base">
-		<Card class="w-60 h-72 overflow-y-auto" :pt="cardPTOptions">
-			<template #subtitle>Clicked features</template>
-			<template  v-if="props.features !== undefined" #content>
-				<Accordion :multiple="true" :activeIndex="[]" :pt="accordPTOptions">
-                    <AccordionTab headerClass="rounded-lg" v-for="(source, index) in Object.entries(mergedFeatures).map(([name, value]) => ({ name, value }))" :key="index" :pt="accordPTOptions">
+    <div class="w-full text-base">
+        <Card class="w-72 h-72 overflow-y-auto" :pt="cardPTOptions">
+            <template v-if="props.features !== undefined" #content>
+                <Accordion :multiple="true" :activeIndex="[]" :pt="accordPTOptions">
+                    <AccordionTab headerClass="rounded-lg"
+                        v-for="(source, index) in Object.entries(mergedFeatures).map(([name, value]) => ({ name, value }))"
+                        :key="index" :pt="accordPTOptions">
                         <template #header>
                             <span class="capitalize">{{ createDisplayName(source.name) }}</span>
                         </template>
-                        <div class="max-h-60 overflow-y-auto divide-y-2">
-							<div v-for="(feature, ind) in source.value" :key="ind" class="p-2 odd:bg-gray-100">
-								<p v-for="(property,i) in Object.entries(feature.properties).map(([name, value]) => ({ name, value }))" :key="i">
-									<span class="font-bold">{{ property.name }}</span>: <span>{{ property.value }}</span>
-								</p>
-							</div>
-						</div>
+                        <div class="max-h-60 overflow-y-auto">
+                            <div v-for="(feature, ind) in source.value" :key="ind" class="rounded-md border mt-1 px-1 first:mt-0 odd:bg-gray-100 divide-y-2 divide-dashed">
+                                <div v-for="(property, i) in Object.entries(feature.properties).map(([name, value]) => ({ name, value }))"
+                                    :key="i">
+                                    <p class="font-bold">{{ property.name }}</p>
+                                    <p class="font-normal italic text-sm">{{ property.value }}</p>
+                                </div>
+                            </div>
+                        </div>
                     </AccordionTab>
                 </Accordion>
-			</template>
-		</Card>
-	</div>
+            </template>
+        </Card>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -34,13 +37,13 @@ import { type MapGeoJSONFeature } from "maplibre-gl";
 
 const mapStore = useMapStore()
 interface Props {
-    features: MapGeoJSONFeature[]|undefined
+    features: MapGeoJSONFeature[] | undefined
 }
 type GroupedFeatures = Record<string, MapGeoJSONFeature[]>;
 const props = defineProps<Props>()
 
-const mergedFeatures = computed(()=>{
-    if (props.features !== undefined){
+const mergedFeatures = computed(() => {
+    if (props.features !== undefined) {
         const groupedFeatures = props.features.reduce<GroupedFeatures>((acc, feature) => {
             const source = feature.source;
             // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
@@ -54,9 +57,9 @@ const mergedFeatures = computed(()=>{
     }
     return []
 })
-function createDisplayName(source: string): string{
-    const layer = mapStore.layersOnMap.filter((layer)=>{ return source === layer.source })[0]
-    if (layer !== undefined){
+function createDisplayName(source: string): string {
+    const layer = mapStore.layersOnMap.filter((layer) => { return source === layer.source })[0]
+    if (layer !== undefined) {
         return (layer.displayName !== undefined && layer.displayName !== "") ? layer.displayName.replaceAll("_", " ") : layer.source.replaceAll("_", " ")
     } else {
         return "-x-x-x-"
@@ -65,6 +68,4 @@ function createDisplayName(source: string): string{
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
