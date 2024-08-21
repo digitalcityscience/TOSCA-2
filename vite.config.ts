73bd00399105552d162839d7dd3637fc4da1a55f
Gokturk <gokturkbrk@gmail.com>
2024-08-22 01:04:55 +0200

    refactor: Remove unnecessary logging statement in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
               console.log('Sending Request to the Target:', req.method, req.url);
             });
             proxy.on('proxyRes', (proxyRes, req, _res) => {
-              console.log('Received Response from the Target:', proxyRes.statusCode, req.url, proxyRes.url);
+              console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
             });
           },
         },
