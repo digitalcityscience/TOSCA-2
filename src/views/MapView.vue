c2d25a244a8cb2e5136123b2b97c7e0c0ec67ec6
Gokturk <gokturkbrk@gmail.com>
2024-08-29 18:40:41 +0200

    add buffer feature

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -4,6 +4,7 @@
         <WorkspaceListing :workspaces="geoserverStore.workspaceList"></WorkspaceListing>
         <MapLayerListing></MapLayerListing>
         <DrawContainer></DrawContainer>
+        <BufferContainer></BufferContainer>
         <Toast/>
     </div>
 </template>
@@ -16,6 +17,7 @@ import MapContainer from "@components/Map/MapContainer.vue";
 const WorkspaceListing = defineAsyncComponent(async () => await import("@components/Data/Workspace/WorkspaceListing.vue"));
 const MapLayerListing = defineAsyncComponent(async () => await import("@components/Map/Layer/MapLayerListing.vue"));
 const DrawContainer = defineAsyncComponent(async () => await import("@components/Map/Layer/Draw/DrawContainer.vue"))
+const BufferContainer = defineAsyncComponent(async () => await import("@components/Map/Buffer/BufferContainer.vue"))
 const Toast = defineAsyncComponent(async () => await import("primevue/toast"))
 const geoserverStore = useGeoserverStore()
 onMounted(()=>{
