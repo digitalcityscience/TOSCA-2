652a50e8faec23566c58d01b3da20f6d7be3133a
Gokturk <gokturkbrk@gmail.com>
2024-10-23 14:18:44 +0200

    set visibility of sidebars based on url info

--- src/components/Participation/ParticipationSidebar.vue
+++ src/components/Participation/ParticipationSidebar.vue
@@ -1,5 +1,5 @@
 <template>
-	<BaseSidebarComponent :id="sidebarID" position="left" :collapsed="true">
+	<BaseSidebarComponent :id="sidebarID" :position="sidebarPosition" :collapsed="true">
 		<template #header>
 			<p>Citizen Participation Tool</p>
 		</template>
@@ -18,17 +18,45 @@
 import BaseSidebarComponent from "@components/Base/BaseSidebarComponent.vue"
 import { SidebarControl } from "../../core/helpers/sidebarControl";
 import { useMapStore } from "../../store/map";
-import { RouterLink } from "vue-router";
+import { RouterLink, useRoute } from "vue-router";
 import Button from "primevue/button";
+import { onMounted } from "vue";
 
 const mapStore = useMapStore()
 const sidebarID = "participation"
+const sidebarPosition = "left"
 // add participation sidebar control to the map
 const iconElement = document.createElement("span")
 iconElement.classList.add("material-icons-outlined")
 iconElement.textContent = "analytics"
 const sidebarControl = new SidebarControl("", sidebarID, document.createElement("div"), iconElement, 2)
 mapStore.map.addControl(sidebarControl, "top-left")
+
+const route = useRoute()
+onMounted(()=>{
+    setupSidebarVisibility()
+})
+function setupSidebarVisibility(): void {
+    const routeMeta = route.meta;
+    if (routeMeta !== undefined && routeMeta.sidebar !== undefined && routeMeta.sidebar !== "" && routeMeta.sidebar !== null) {
+        const sidebarId = routeMeta.sidebar as string;
+        const position = routeMeta.sidebarPosition as string;
+        const sidebars = document.getElementsByClassName(`sidebar-${position}`)
+        if (sidebars.length > 0){
+            for (let i = 0; i < sidebars.length; i++) {
+                if (sidebars[i].id === sidebarId) {
+                    sidebars[i].classList.remove("collapsed");
+                } else {
+                    sidebars[i].classList.add("collapsed");
+                }
+            }
+        }
+        const sidebarElement = document.getElementById(sidebarId);
+        if (sidebarElement != null) {
+            sidebarElement.classList.remove("collapsed");
+        }
+    }
+}
 </script>
 
 <style scoped></style>
--- src/views/ParticipationView.vue
+++ src/views/ParticipationView.vue
@@ -7,15 +7,9 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
 import ParticipationSidebar from "../components/Participation/ParticipationSidebar.vue"
-import sidebarConfig from "../configurations/sidebars"
 import { useMapStore } from "../store/map"
 const mapStore = useMapStore()
 onMounted(() => {
-    console.log("participation mounted")
-    // rearrange sidebar visibility
-    sidebarConfig.sidebars.forEach((sidebar) => {
-        document.getElementById(sidebar.id)?.classList.toggle("collapsed")
-    })
     // reset all data on the map before adding demo data
     mapStore.resetMapData().then(() => { }, () => { })
 })
