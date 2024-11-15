c8179e264bb922c5aad6afebba23848989f66ede
Dobroslav Totev <dev@totev.de>
2024-11-15 14:13:39 +0100

    feat: add vitetest and jsdom

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
+  resolve: {
     alias: {
       "@components": resolve(__dirname, "./src/components"),
-      "@store":resolve(__dirname,"./src/store"),
-      "@helpers":resolve(__dirname,"./src/core/helpers"),
-      "@presets":resolve(__dirname, "./src/presets")
-    }
+      "@store": resolve(__dirname, "./src/store"),
+      "@helpers": resolve(__dirname, "./src/core/helpers"),
+      "@presets": resolve(__dirname, "./src/presets"),
+    },
   },
-})
+  test: {
+    /* for example, use global to avoid globals imports (describe, test, expect): */
+    globals: true,
+    environment: "jsdom",
+  },
+});
