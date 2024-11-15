b7dbe9ad1a889be611d7d76cb61a70cd53d6a52f
Dobroslav Totev <dev@totev.de>
2024-11-15 14:30:25 +0100

    feat: added snapshot test

--- src/components/Base/BaseSidebarComponent.test.ts
+++ src/components/Base/BaseSidebarComponent.test.ts
@@ -25,3 +25,15 @@ test("should NOT be visible, if initialized with colapsed=true", () => {
     const component = wrapper.find("[id=testId]");
     expect(component.classes()).toContain("collapsed");
 });
+
+test("should produce the correct html", () => {
+    const wrapper = mount(BaseSidebarComponent, {
+        props: {
+            position: "left",
+            id: "testId",
+            collapsed: true,
+        },
+    });
+
+    expect(wrapper.html()).toMatchSnapshot();
+});
