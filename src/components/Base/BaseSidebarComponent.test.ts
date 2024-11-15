59e81197cf8ff5c838576ce59809e7f4d1c26762
Dobroslav Totev <dev@totev.de>
2024-11-15 14:20:36 +0100

    feat: component test

--- src/components/Base/BaseSidebarComponent.test.ts
+++ src/components/Base/BaseSidebarComponent.test.ts
@@ -13,3 +13,15 @@ test("should be visible, if initialized with colapsed=false", () => {
     const component = wrapper.find("[id=testId]");
     expect(component.classes()).not.toContain("collapsed");
 });
+
+test("should NOT be visible, if initialized with colapsed=true", () => {
+    const wrapper = mount(BaseSidebarComponent, {
+        props: {
+            position: "left",
+            id: "testId",
+            collapsed: true,
+        },
+    });
+    const component = wrapper.find("[id=testId]");
+    expect(component.classes()).toContain("collapsed");
+});
