b21020f4f4d63698d9548710d0f1f7e2fcc4da49
Gokturk <gokturkbrk@gmail.com>
2025-02-13 14:45:49 +0100

    add typedoc functionality to the project

--- src/configurations/api/geonodeConfig.ts
+++
@@ -1,9 +0,0 @@
-const GEONODE_API_BASE_URL = import.meta.env.VITE_GEONODE_REST_URL;
-export const CAMPAIGNS_URL = `${GEONODE_API_BASE_URL}/v2/cpt/campaigns/`;
-export const CAMPAIGN_FEEDBACK_URL = `${GEONODE_API_BASE_URL}/v2/cpt/campaigns/feedback/`;
-export const getCampaignDetailUrl = (campaignURL: string): string => {
-    return `${CAMPAIGNS_URL}/${campaignURL}`;
-};
-export const getFeedbackUrl = (campaignURL: string): string => {
-    return `${CAMPAIGNS_URL}/${campaignURL}/feedback/`;
-}
